4885a34f7fa86457ade22599ab85fd7e2d1f5100
Aaron Quint <aaron@quirkey.com>
2009-03-01 23:44:21 -0500

    First pages commit

--- lib/static_model/errors.rb
+++
@@ -1,7 +0,0 @@
-module StaticModel
-  class Error < RuntimeError; end;
-  class RecordNotFound < Error; end;
-  class DataFileNotFound < Error; end;
-  class BadDataFile < Error; end;
-  class BadOptions < Error; end;
-end
