5aa0567d40eb674928234f93dedcce4e3c80f58b
Aaron Quint <aaron@quirkey.com>
2011-08-23 17:55:05 -0400

    Version bump

--- lib/static_model.rb
+++ lib/static_model.rb
@@ -5,7 +5,7 @@ require 'yaml' unless defined?(YAML)
 require 'erb'  unless defined?(ERB)
 
 module StaticModel
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 end
 
 require 'static_model/active_support'
